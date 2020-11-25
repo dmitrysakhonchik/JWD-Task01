ce107f575246d4d1e09334765a8411285deaff9a
Dmitry Sakhonchik <dm.sakhonchik@gmail.com>
2020-11-25 19:33:11 +0300

    add test for first subtask

--- src/main/java/by/sakhonchik/task01/subtask01/LastDigitOfASquare.java
+++ src/main/java/by/sakhonchik/task01/subtask01/LastDigitOfASquare.java
@@ -1,32 +1,25 @@
 package by.sakhonchik.task01.subtask01;
 
-import java.util.Scanner;
-
 /**
  * @author Dmitry Sakhonchik
  */
 public class LastDigitOfASquare {
-    private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        getLastDigitOfASquare();
-    }
 
-    public static void getLastDigitOfASquare() {
-        String numberEnteredByUser;
-        System.out.println("Enter number:");
-        while (!isCorrectDigit(numberEnteredByUser = scanner.nextLine())) {
-            System.out.println("Incorrect value");
-            System.out.println("Enter number:");
+    public double getLastDigitOfASquare(String number) {
+        double answer = 0;
+        if (isCorrectDigit(number)) {
+            double lastDigitOfNumber = Double.parseDouble(number) % 10;
+            answer = lastDigitOfNumber * lastDigitOfNumber % 10;
         }
-        int lastDigitOfNumber = Integer.parseInt(numberEnteredByUser) % 10;
-        System.out.println("answer is  : " + lastDigitOfNumber * lastDigitOfNumber % 10);
+
+        return answer;
 
     }
 
-    public static boolean isCorrectDigit(String numberEnteredByUser) {
+    public boolean isCorrectDigit(String numberEnteredByUser) {
         try {
-            int number = Integer.parseInt(numberEnteredByUser);
+            double number = Double.parseDouble(numberEnteredByUser);
             return number != 0;
 
         } catch (NumberFormatException exception) {
--- src/test/java/by/sakhonchik/task01/subtask01/LastDigitOfASquareTest.java
+++ src/test/java/by/sakhonchik/task01/subtask01/LastDigitOfASquareTest.java
@@ -1,20 +1,42 @@
 package by.sakhonchik.task01.subtask01;
 
-import junit.framework.TestCase;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class LastDigitOfASquareTest extends TestCase {
+/**
+ * @author Dmitry Sakhonchik
+ */
+public class LastDigitOfASquareTest {
+    private LastDigitOfASquare lastDigitOfASquare;
+
+    @Before
+    public void setUp() {
+        lastDigitOfASquare = new LastDigitOfASquare();
+    }
 
 
     @Test
     public void testGetLastDigitOfASquare() {
-        int x = 222;
-        int actual;
+
+        double actual;
         int expected = 4;
 
-        LastDigitOfASquare.getLastDigitOfASquare();
-        Assert.assertEquals(expected,actual);
+        actual = lastDigitOfASquare.getLastDigitOfASquare("22");
+        Assert.assertEquals(expected, actual, 1e-9);
     }
 
+    @Test
+    public void thisIsANumber() {
+        boolean actual;
+        actual = lastDigitOfASquare.isCorrectDigit("22");
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void thisIsNotANumber() {
+        boolean actual;
+        actual = lastDigitOfASquare.isCorrectDigit("qwe");
+        Assert.assertFalse(actual);
+    }
 }
